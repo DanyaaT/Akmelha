3957c3af888d8a0e2e347ae731b7aa91f88ee6b1
Hadeel <hadeellm@outlook.com>
2023-02-03 13:52:43 +0300

    .

--- Akmelha/Student/StudentGame/PuanlamaView.swift
+++ Akmelha/Student/StudentGame/PuanlamaView.swift
@@ -2,35 +2,28 @@
 //  PuanlamaView.swift
 //  Akmelha
 //
-//  Created by Hadeel Mohammed on 10/07/1444 AH.
+//  Created by Hadeel Mohammed on 12/07/1444 AH.
 //
+
 
 import SwiftUI
 struct PuanlamaView: View {
-    let rekorName: String
-    let rekorPuan: Double
+   
+ 
     let geçenSüre: Double
     let hareketSayacı: Int
     
     var body: some View {
         VStack{
 
-            HStack{
-                Text(rekorName)
-                Text(String(format: "Rekor: %.2f" , rekorPuan))
-            }
             VStack {
                 HStack{
-                    Text(String(format: "Geçen Süre: %.2f", geçenSüre))
+                    Text(String(format: "عدد الثواني : %.2f", geçenSüre))
                     Spacer()
-                    Text("\(hareketSayacı)")
+                    Text("عدد الحركات : \(hareketSayacı)")
                 }
             }
         }.padding(.horizontal)
     }
 }
-struct PuanlamaView_Previews: PreviewProvider {
-    static var previews: some View {
-        PuanlamaView(rekorName: "rekon", rekorPuan: 23.5, geçenSüre: 3.5, hareketSayacı: 3).previewLayout(.sizeThatFits)
-    }
-}
+
